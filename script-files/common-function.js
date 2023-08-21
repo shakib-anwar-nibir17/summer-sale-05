bce1a73ddebd084eba26b2387e0380e03ee92334
shakib-anwar-nibir17 <nibirshakib@gmail.com>
2023-08-21 12:39:24 +0600

    function reusability improved

--- script-files/common-function.js
+++ script-files/common-function.js
@@ -1,5 +1,5 @@
 // string to number conversion
-function inputPrice(inputId){
+function inputPrice(inputId) {
   const inputPriceField = document.getElementById(inputId); // can be product price or total price
   const inputPriceString = inputPriceField.innerText;
   const inputPrice = parseFloat(inputPriceString);
@@ -7,92 +7,95 @@ function inputPrice(inputId){
 }
 
 // setting the value
-function setValue(inputId, value){
+function setValue(inputId, value) {
   const valueInString = parseFloat(value);
   const valueFormatted = valueInString.toFixed(2);
   document.getElementById(inputId).innerText = valueFormatted;
 }
 
-function addToProductEntry(inputId){
-  const productEntry = document.getElementById('product-entry');
+// ADDING PRODUCT NAME TO CART
+function addToProductEntry(inputId) {
+  const productEntry = document.getElementById("product-entry");
   const count = productEntry.childElementCount;
 
-  const p = document.createElement('p');
-  p.classList.add = ('my-4');
-  p.innerHTML =` ${count + 1}. ${inputId} `
+  const p = document.createElement("p");
+  p.classList.add = "my-4";
+  p.innerHTML = ` ${count + 1}. ${inputId} `;
 
   productEntry.appendChild(p);
 }
 
-
-
-function enablePurchaseButton(){
-  const validationString = document.getElementById('total-price').innerText;
+// main card function
+function productCardClicked(cardPriceId, productName) {
+  const productPrice = inputPrice(cardPriceId);
+  const totalPricePrevious = inputPrice("total-price");
+  const newTotalPrice = totalPricePrevious + productPrice;
+  addToProductEntry(productName);
+  setValue("total-price", newTotalPrice);
+  setValue("total", newTotalPrice);
+  enablePurchaseButton();
+  enableCouponButton();
+}
+// condition to enable purchase button
+function enablePurchaseButton() {
+  const validationString = document.getElementById("total-price").innerText;
   const validation = parseFloat(validationString);
-  const purchaseButton = document.getElementById('purchase-btn');
-  if( validation > 0){
-    purchaseButton.removeAttribute('disabled');
+  const purchaseButton = document.getElementById("purchase-btn");
+  if (validation > 0) {
+    purchaseButton.removeAttribute("disabled");
+  } else {
+    purchaseButton.setAttribute("disabled", true);
   }
-  else{
-    purchaseButton.setAttribute('disabled', true);
-  }
-  
 }
 
-function enableCouponButton(){
-  const validationString = document.getElementById('total-price').innerText;
+// condition to enable coupon button
+function enableCouponButton() {
+  const validationString = document.getElementById("total-price").innerText;
   const validation = parseFloat(validationString);
-  const purchaseButton = document.getElementById('coupon-button');
-  if( validation > 200){
-    purchaseButton.removeAttribute('disabled');
+  const purchaseButton = document.getElementById("coupon-button");
+  if (validation >= 200) {
+    purchaseButton.removeAttribute("disabled");
+  } else {
+    purchaseButton.setAttribute("disabled", true);
   }
-  else{
-    purchaseButton.setAttribute('disabled', true);
-  }
-  
 }
 
-function applyCoupon(){
-  document.getElementById('coupon-button').addEventListener('click', function(){
-    const couponField = document.getElementById('coupon-field');
-    const couponCode = couponField.value;
-    if( couponCode === 'SELL200'){
-      couponField.value = '';
-      const totalPrice = document.getElementById('total-price').innerText;
-       const totalPriceValue = parseFloat(totalPrice);
-       const discountPrice = totalPriceValue * 0.2
-       const total = totalPriceValue - discountPrice;
-       setValue('discount-amount', discountPrice);
-        setValue('total', total);
-    }
-    else{
-      couponField.value = '';
-      return;
-    }
-  })
+// rating system propagation stopping
+
+function stopRatingClickPropagation(event) {
+  event.stopPropagation();
 }
 
-// function applyCoupon() {
-//   const couponField = document.getElementById('coupon-field');
-//   const couponButton = document.getElementById('coupon-button');
-  
-//   couponButton.onclick = function() {
-//     const couponCode = couponField.value;
-    
-//     if (couponCode === 'SELL200') {
-//       couponField.value = '';
-//       const totalPrice = document.getElementById('total-price').innerText;
-//       const totalPriceValue = parseFloat(totalPrice);
-//       const discountPrice = totalPriceValue * 0.2;
-//       const total = totalPriceValue - discountPrice;
-      
-//       setValue('discount-amount', discountPrice);
-//       setValue('total', total);
-//     } else {
-//       couponField.value = ''; 
-//       alert('Invalid Coupon Code'); 
-//     }
-//   };
-// }
+const ratingInputs = document.querySelectorAll('.mask');
 
+for (let i = 0; i < ratingInputs.length; i++) {
+  ratingInputs[i].addEventListener('click', stopRatingClickPropagation);
+}
 
+// go home button
+document.getElementById("go-home").addEventListener("click", function () {
+  document.getElementById("product-entry").innerHTML = "";
+  document.getElementById("total-price").innerText = "";
+  document.getElementById("discount-amount").innerText = "";
+  document.getElementById("total").innerText = "";
+  window.location.href = "index.html";
+});
+
+// coupon apply button
+document.getElementById("coupon-button").addEventListener("click", function () {
+  const couponField = document.getElementById("coupon-field");
+  const couponCode = couponField.value;
+  if (couponCode === "SELL200") {
+    couponField.value = "";
+    const totalPrice = document.getElementById("total-price").innerText;
+    const totalPriceValue = parseFloat(totalPrice);
+    const discountPrice = totalPriceValue * 0.2;
+    const total = totalPriceValue - discountPrice;
+    setValue("discount-amount", discountPrice);
+    setValue("total", total);
+  } else {
+    couponField.value = "";
+    alert("Invalid Coupon Code");
+    return;
+  }
+});
--- script-files/main.js
+++ script-files/main.js
@@ -1,78 +1,38 @@
-document.getElementById('card-one').addEventListener('click', function(){
-  
-  const productPrice = inputPrice('price-one')
-  const totalPricePrevious = inputPrice('total-price')
-  const newTotalPrice = totalPricePrevious+ productPrice;
-  addToProductEntry('Kitchen Gadgets');
-  setValue('total-price', newTotalPrice);
-  enablePurchaseButton();
-  enableCouponButton();
-  applyCoupon();
-}) 
+document.getElementById("card-one").addEventListener("click", function () {
+  productCardClicked("price-one", "Kitchen Gadgets");
+});
 
-document.getElementById('card-two').addEventListener('click', function(){
-  const productPrice = inputPrice('price-two')
-  const totalPricePrevious = inputPrice('total-price')
-  const newTotalPrice = totalPricePrevious+ productPrice;
-  addToProductEntry('Serving Items');
-  setValue('total-price', newTotalPrice);
-  enablePurchaseButton();
-  enableCouponButton();
-  applyCoupon();
-})  
+document.getElementById("card-two").addEventListener("click", function () {
+  productCardClicked("price-two", "Serving Items");
+});
 
-document.getElementById('card-three').addEventListener('click', function(){
-  const productPrice = inputPrice('price-three')
-  const totalPricePrevious = inputPrice('total-price')
-  const newTotalPrice = totalPricePrevious+ productPrice;
-  setValue('total-price', newTotalPrice);
-  addToProductEntry('Cooking Pots');
-  enablePurchaseButton();
-  enableCouponButton();
-  applyCoupon();
-})
+document.getElementById("card-three").addEventListener("click", function () {
+  productCardClicked("price-three", "Cooking Pots");
+});
 
-document.getElementById('card-four').addEventListener('click', function(){
-  const productPrice = inputPrice('price-four')
-  const totalPricePrevious = inputPrice('total-price')
-  const newTotalPrice = totalPricePrevious+ productPrice;
-  addToProductEntry('Sports Back Cap');
-  setValue('total-price', newTotalPrice);
-  enablePurchaseButton();
-  enableCouponButton();
-  applyCoupon();
-})
+document.getElementById("card-four").addEventListener("click", function () {
+  productCardClicked("price-four", "Sports Back Cap");
+});
 
-document.getElementById('card-five').addEventListener('click', function(){
-  const productPrice = inputPrice('price-five')
-  const totalPricePrevious = inputPrice('total-price')
-  const newTotalPrice = totalPricePrevious+ productPrice;
-  addToProductEntry('Full Jersey Set');
-  setValue('total-price', newTotalPrice);
-  enablePurchaseButton();
-  enableCouponButton();
-  applyCoupon();
-})
+document.getElementById("card-five").addEventListener("click", function () {
+  productCardClicked("price-five", "Full Jersey Set");
+});
 
-document.getElementById('card-six').addEventListener('click', function(){
-  const productPrice = inputPrice('price-six')
-  const totalPricePrevious = inputPrice('total-price')
-  const newTotalPrice = totalPricePrevious+ productPrice;
-  addToProductEntry('Sports cates');
-  setValue('total-price', newTotalPrice);
-  enablePurchaseButton();
-  enableCouponButton();
-  applyCoupon();
-})
+document.getElementById("card-six").addEventListener("click", function () {
+  productCardClicked("price-six", "Sports cates");
+});
 
-document.getElementById('go-home').addEventListener('click', function(){
-  document.getElementById('product-entry').innerHTML = '';
-  document.getElementById('total-price').innerText = '';
-  document.getElementById('discount-amount').innerText = '';
-  document.getElementById('total').innerText = '';
-  window.location.href = 'index.html';
-})
+document.getElementById("card-seven").addEventListener("click", function () {
+  productCardClicked("price-seven", "Single Relax Chair");
+});
 
+document.getElementById("card-eight").addEventListener("click", function () {
+  productCardClicked("price-eight", "Children play");
+});
 
+document.getElementById("card-nine").addEventListener("click", function () {
+  productCardClicked("price-nine", "Flexible Sofa");
+});
+ 
 
 
